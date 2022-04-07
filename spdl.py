ead67b81acb7068c60afd7019f7c92fc00b88f7b
mashuptwice <info@mashup-tech.de>
2022-04-07 09:39:34 +0000

    fixes

--- spdl.py
+++ spdl.py
@@ -1,30 +1,44 @@
 #!/usr/bin/env python
-import time, requests, os, subprocess, re, yt_dlp, natsort, shutil
+import requests, os, subprocess, re, yt_dlp, natsort, shutil
 
 url_base_en="https://www.southpark.de/en/seasons/south-park"
 
+#create download directories
 os.makedirs("dl", exist_ok=True)
 os.makedirs("dl/tmp", exist_ok=True)
 
+#request webpage
 res_seasons = requests.get(url_base_en)
 res_seasons.raise_for_status()
 
-#get the "magic" string of the season
-#\\u002F(?P<code>[a-zA-Z0-9]+)\\u002Fseason-
+#get the url of the season
+seasonexp = "\\u002F(?P<seasoncode>[a-zA-Z0-9]+)\\u002F(?P<seasonname>season-\d{1,2})"
 
+seasonnames = []
+seasonurls = []
 
-seasonnr = input("Which season do you want to download: ")
+#dirty fix to include the newest season
+seasonnames.append("newest")
+seasonurls.append("https://www.southpark.de/en/seasons/south-park")
 
-seasonexp = "\\u002F(?P<code>[a-zA-Z0-9]+)\\u002Fseason-"
-matches=re.search(seasonexp + seasonnr, res_seasons.text)
-seasoncode=matches.group("code")
-#print(seasoncode)
-#craft a url
-url="https://www.southpark.de/en/seasons/south-park/" + seasoncode + "/season-" + seasonnr
-#print(url)
+for i in re.finditer(seasonexp, res_seasons.text):
+	seasonnames.append(i.group("seasonname"))
+	seasonurls.append("https://www.southpark.de/en/seasons/south-park/" + i.group("seasoncode") + "/" + i.group("seasonname"))
+
+#reverse lists
+seasonnames.reverse()
+seasonurls.reverse()
+
+count = 1
+for i in seasonnames:
+        print(str(count) + ":  " + i)
+        count += 1
+
+#get user input
+seasonnr = input("Select one season to download: ")
 
 #get the episodes urls 
-res_episodes = requests.get(url)
+res_episodes = requests.get(seasonurls[int(seasonnr)-1])
 res_episodes.raise_for_status()
 episodeexp = "href=\"(?P<episodeurl>/en/episodes/[a-z0-9]+/(?P<episodename>[a-z0-9-]+))\""
 
@@ -36,38 +50,31 @@ for i in re.finditer(episodeexp, res_episodes.text):
         episodeurls.append("https://southpark.de" + i.group("episodeurl"))
         episodenames.append(i.group("episodename"))
 
-#print(episodeurls)
-#print(episodenames)
-
 count = 1
 for i in episodenames:
         print(str(count) + ":  " + i)
         count += 1
+#get user input
+episodenr = input("Select one episode to download: ")
 
-
-episodenr = input("select one episode to download: "
-        )
 #download the episode
-
 os.chdir("dl/tmp")
-
 ydl_opts = {}
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download(episodeurls[int(episodenr)-1])
-
 
 #the episodes get downloaded as multiple files. Prepare a list of all parts for further processing with ffmpeg
 try:
         os.remove("list.txt")
 except OSError:
         pass
+
 files = natsort.natsorted(os.listdir())
 f = open("list.txt", "a")
 for i in files:
         f.write("file " + "\'" + i + "\'" + "\n")
 f.write("\n")
 f.close()
-
 
 #concat using ffmpeg
 subprocess.Popen(['ffmpeg', '-f', 'concat', '-safe', '0', '-i', 'list.txt', '-c', 'copy', 'out.mp4']).wait()
